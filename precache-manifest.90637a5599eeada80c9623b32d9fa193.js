29b14c65ba3d1b080f90d7ded5bec9a5b3a04981
PeterBrw <peter.giurgiu@gmail.com>
2020-02-20 11:50:52 +0200

    Updates

---
+++ precache-manifest.90637a5599eeada80c9623b32d9fa193.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77de59e2ad6792d58225ecda849af119",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "a761ef0c938d85d637a6",
+    "url": "/react-tutorial/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6edcdf994bf5c4113157",
+    "url": "/react-tutorial/static/js/2.1ceb358c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-tutorial/static/js/2.1ceb358c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a761ef0c938d85d637a6",
+    "url": "/react-tutorial/static/js/main.6b3f3287.chunk.js"
+  },
+  {
+    "revision": "d1a4ad59d776d93e8cfb",
+    "url": "/react-tutorial/static/js/runtime-main.4b063f4c.js"
+  }
+]);
